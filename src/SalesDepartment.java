4e9b0947d13aa7ecaa9edf956f709f9046d06791
kpilszak <klaudia.pilszak@gmail.com>
2019-03-10 11:33:44 +0100

    Add constructors

--- src/SalesDepartment.java
+++ src/SalesDepartment.java
@@ -3,6 +3,11 @@ public class SalesDepartment implements Department {
     private Integer id;
     private String name;
 
+    public SalesDepartment(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     @Override
     public void printDepartmentName() {
 
